562c914320a171787829fff7994979b3450016e3
Carl Robinson Jr <CarlRobinsonJr@gmail.com>
2019-11-02 05:39:20 -0400

    created layouts for blades

--- todoapp/resources/views/todos/index.blade.php
+++ todoapp/resources/views/todos/index.blade.php
@@ -1,14 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css' integrity='sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO' crossorigin='anonymous'>
-    
-    <title>Todos</title>
-</head>
-<body>
+@extends('layouts.app')
+@section('title')
+Todo List
+@stop
+@section('content')
 <div class="container">
 <h1 class="text-center my-5">TODOS Page</h1>
  <div class="row justify-content-center">
@@ -33,5 +27,4 @@
  
  </div>
  </div>
-</body>
-</html>
+@stop
