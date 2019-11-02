5b2550bbc55cead4f83bac7b8df19bfc67fcedd2
Carl Robinson Jr <CarlRobinsonJr@gmail.com>
2019-11-02 04:41:20 -0400

    added bootstrap and styling

--- todoapp/resources/views/todos/index.blade.php
+++ todoapp/resources/views/todos/index.blade.php
@@ -4,16 +4,34 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css' integrity='sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO' crossorigin='anonymous'>
+    
     <title>Todos</title>
 </head>
 <body>
-    <h1>Todos Page</h1>
-
+<div class="container">
+<h1 class="text-center my-5">TODOS Page</h1>
+ <div class="row justify-content-center">
+ <div class="col-md-8">
+ <div class="card card-default">
+    <div class="card-header">
+    Todos
+    </div>
+    <div class="card-body">
+    <ul class="list-group">
     @foreach($todos as $todo)
-    <li>
+    <li class="list-group-item">
     {{$todo->name}}
+    <button class="btn btn-primary btn-sm float-right">View</button>
     </li>
     @endforeach
-
+    </ul>
+    </div>
+    </div>
+    </div>
+ 
+ 
+ </div>
+ </div>
 </body>
 </html>
